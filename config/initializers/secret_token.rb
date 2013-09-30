9db98e548a7a5bd5267c71a9a7ea19e4139afbc8
Jason Adam Young <jayoung@extension.org>
2013-09-30 08:20:51 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Search::Application.config.secret_token = '7851414bbbde72793620c19c7d6b52def0e76f0a603e715683b961c7d45d6978cfe50d30faf971d61e24847be2257d144fa4ea9accf2babc47259fba2fc6e433'
