cdba5ae302e438a2051067c5ad67e9b7ed34af87
Jason Adam Young <jayoung@extension.org>
2013-09-30 17:44:47 -0400

    initial views routes, and styles

---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,11 @@
+# === COPYRIGHT:
+#  Copyright (c) North Carolina State University
+#  Developed with funding for the National eXtension Initiative.
+# === LICENSE:
+#  BSD(-compatible)
+#  see LICENSE file
+
+class HomeController < ApplicationController
+  def index
+  end
+end
