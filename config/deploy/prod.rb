b34f80dbbc6b8a3836a1571b4a85d9706b8de465
Jason Adam Young <jayoung@extension.org>
2013-10-02 11:36:58 -0400

    point to search even if dev-search still is the same

--- config/deploy/prod.rb
+++ config/deploy/prod.rb
@@ -1,3 +1,3 @@
 set :deploy_to, "/services/search/"
 set :branch, 'master'
-server 'dev-search.extension.org', :app, :web, :db, :primary => true
+server 'search.extension.org', :app, :web, :db, :primary => true
