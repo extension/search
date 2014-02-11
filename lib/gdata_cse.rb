e6688e2d44a67a00d422c032dde6f7698b486a32
Jason Adam Young <jayoung@extension.org>
2014-02-11 11:58:32 -0500

    properly indicate licensing terms

--- lib/gdata_cse.rb
+++ lib/gdata_cse.rb
@@ -2,7 +2,7 @@
 #  Copyright (c) 2005-2010 North Carolina State University
 #  Developed with funding for the National eXtension Initiative.
 # === LICENSE:
-#  BSD(-compatible)
+#  
 #  see LICENSE file or view at http://about.extension.org/wiki/LICENSE
 
 require 'gdata'
