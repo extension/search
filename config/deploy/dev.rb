2bf80b417c09c397c7bbaaec3e3864617836263b
Jason Adam Young <jayoung@extension.org>
2014-04-22 14:38:33 -0400

    search has no branches, one day it might

--- config/deploy/dev.rb
+++ config/deploy/dev.rb
@@ -1,3 +1,7 @@
 set :deploy_to, "/services/search/"
-set :branch, 'master'
+if(branch = ENV['BRANCH'])
+  set :branch, branch
+else
+  set :branch, 'master'
+end
 server 'dev-search.extension.org', :app, :web, :db, :primary => true
