5995fb3a40d437816c8153284cf0506871661ca8
Jason Young <jay@outfielding.net>
2017-10-13 13:57:15 -0400

    Deployment changes

--- config/deploy/prod.rb
+++ config/deploy/prod.rb
@@ -1,5 +1,5 @@
 set :deploy_to, "/services/search/"
 set :branch, 'master'
 set :vhost, 'search.extension.org'
-set :deploy_server, 'search.aws.extension.org'
+set :deploy_server, 'search.awsi.extension.org'
 server deploy_server, :app, :web, :db, :primary => true
